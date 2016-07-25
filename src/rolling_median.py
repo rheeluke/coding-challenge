f197aefeae3f18535fc847c4990e236a56a90b40
Luke Rhee <luke.rhee@nintex.com>
2016-07-24 23:05:05 -0700

    optimize runtime

--- src/rolling_median.py
+++ src/rolling_median.py
@@ -4,40 +4,27 @@ import json
 from median_degree import VenmoPayments
 
 
-def process_venmo_payments(finName='./venmo_input/venmo-trans.txt',
-                           foutName='./venmo_output/output.txt'):
+def process_venmo_payments(finName, foutName):
     """Process payment data, and write to file streaming median degree."""
-    with open(finName, 'r') as fin:
-        with open(foutName, 'w') as fout:
-            paymentDict = {}
-            jsonString = fin.readline()
-            while jsonString:
-                paymentDict = json.loads(jsonString)
-                jsonString = fin.readline()
-                try:
-                    paymentDict['actor']
-                    paymentDict['target']
-                    paymentDict['created_time']
-                    break
-                except KeyError:
-                    pass
+    with open(finName, 'r') as fin, open(foutName, 'w') as fout:
+        venmoGraph = VenmoPayments()
 
-            venmoGraph = VenmoPayments(paymentDict)
+        jsonString = fin.readline()
+        while jsonString:
+            paymentDict = json.loads(jsonString)
+            try:
+                paymentDict['actor']
+                paymentDict['target']
+                paymentDict['created_time']
+            except KeyError:
+                continue
+
+            venmoGraph.add_payment(paymentDict)
             fout.write('%.2f' % venmoGraph.medianDegree)
 
-            while jsonString:
-                paymentDict = json.loads(jsonString)
-                jsonString = fin.readline()
-                try:
-                    paymentDict['actor']
-                    paymentDict['target']
-                    paymentDict['created_time']
-                except KeyError:
-                    continue
-
-                venmoGraph.add_payment(paymentDict)
-                fout.write('\n%.2f' % venmoGraph.medianDegree)
-
+            jsonString = fin.readline()
+            if jsonString:
+                fout.write('\n')
 
 if __name__ == '__main__':
     process_venmo_payments(sys.argv[1], sys.argv[2])
